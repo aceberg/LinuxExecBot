0e8d95765d408a2fb0040356fed795c112e58231
aceberg <1502200+aceberg@users.noreply.github.com>
2022-12-02 23:08:10 +0700

    Build deb package

---
+++ build/ci/build-deb.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+PKGDIR=LinuxExecBot-$1-0_all
+
+mkdir -p $PKGDIR/usr/bin
+chmod 755 LinuxExecBot
+cp LinuxExecBot $PKGDIR/usr/bin/
+
+mkdir -p $PKGDIR/DEBIAN
+
+echo "Package: linuxexecbot
+Version: $1
+Section: utils
+Priority: optional
+Architecture: all
+Maintainer: aceberg <aceberg_a@proton.me>
+Description: Telegram bot to execute a command from a configurable list on your Linux machine
+" > $PKGDIR/DEBIAN/control
+
+dpkg-deb --build --root-owner-group $PKGDIR
